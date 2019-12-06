660591e8075eb311df64bcc3450acd6346c1d20c
Mateusz Sikora <ms1qaz@gmail.com>
2019-12-06 13:02:40 +0100

    simple webview

--- android-sdk/src/main/java/com/example/android_sdk/TipserSdk.kt
+++ android-sdk/src/main/java/com/example/android_sdk/TipserSdk.kt
@@ -1,6 +1,23 @@
 package com.example.android_sdk
 
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
 class TipserSdk {
+
+    fun addToCart() {
+
+    }
+
+    fun initWebView(activityContext: AppCompatActivity) {
+        val myWebView = WebView(activityContext)
+        myWebView.webViewClient = WebViewClient()
+        activityContext.setContentView(myWebView)
+        myWebView.settings.domStorageEnabled = true
+        myWebView.settings.javaScriptEnabled = true
+        myWebView.loadUrl("https://tipser.com")
+    }
 
     fun goToCheckout() {
 
--- app/src/main/java/com/example/android_sdk_bootstrap/MainActivity.kt
+++ app/src/main/java/com/example/android_sdk_bootstrap/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.android_sdk_bootstrap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 import com.example.android_sdk.TipserSdk
 
 
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val sdk = TipserSdk()
-        sdk.init()
+
+        sdk.initWebView(this)
     }
 }
