1e40b4d59382e8811a393c7028fa60ea15be363e
Mateusz Sikora <ms1qaz@gmail.com>
2019-12-06 17:30:58 +0100

    token logic

--- android-sdk/src/main/java/com/example/android_sdk/TipserSdk.kt
+++ android-sdk/src/main/java/com/example/android_sdk/TipserSdk.kt
@@ -1,22 +1,95 @@
 package com.example.android_sdk
 
+import android.util.Log
+import android.webkit.CookieManager
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.Volley
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.AuthFailureError
 
+
+val BASE_TIPSER_URL = "https://tipser.com"
+val BASE_TIPSER_API_URL = "https://t3-prod-api.tipser.com"
+val TOKEN =
+    "eyJraWQiOm51bGwsImFsZyI6IlJTNTEyIn0.eyJpc3MiOiJUaXBzZXIiLCJqdGkiOiJ3RVhsTUhIZkxvVWdmWjB0QWxsZ2p3IiwiaWF0IjoxNTc1NjM4OTUxLCJleHAiOjE2MDcxNzQ5NTEsImNhcnRJZCI6IjVkZWE1N2E3ZGZhNzNlMDAwMTRjYzU4ZSJ9.RUAheE6_plw4QrLAUh5yyBXpdaIm5OxrXPeou4oaxqwxX_jMeEkn-kyFqE4lWPjr6a3POu_jSoON4QQyZGq9V9Kkean2kbDRhqnUmEHnl4QVRrttXbZ-sJsqHur6cYmjv_ZQl_6A-_lZRJUeilOY9RuXjo_oaZTGq-07-PHwinxCaGx49rmLmYtlPZhO-E-z86nUKg4884VvjSeel_sSaMy_m3PuJ4L8lw-zdPyMkrEQ0SRAuTxDyd40LjXMpw83vH_tQlTzZRv031Vz5iP9_5MCBq2HLYYTSXKoUvNB3sz5dJe7h9z6LiSkR2BFh6LfHX9hL-NbUKu-GeC7Sgpyfw"
+val COOKIE_DOMAIN = "www.tipser.com"
+val TOKEN_INVALID = "TOKEN_INVALID"
+val TIPSER_TOKEN_KEY = "tipserToken"
+
+// 1. add product to cart => response ma token
 class TipserSdk {
+    private lateinit var webView: WebView
+    private lateinit var queue: RequestQueue
 
-    fun addToCart() {
+    fun addToCart(productId: String) {
+        this.webView.loadUrl("https://www.tisper.com/wi/5075d7715c3d090a90585e87/direct-to-checkout?productId=$productId")
+    }
 
+    fun init(activityContext: AppCompatActivity) {
+        this.queue = Volley.newRequestQueue(activityContext)
+        CookieManager.getInstance().setAcceptCookie(true)
+        val getNewToken = fun() {
+            val jsObjRequest = StringRequest(
+                Request.Method.GET,
+                "$BASE_TIPSER_API_URL/v3/auth/anonymousToken",
+                Response.Listener<String> { response ->
+                    CookieManager.getInstance().setCookie(COOKIE_DOMAIN, "tipserToken=$response;")
+                },
+                Response.ErrorListener { e -> e.printStackTrace() })
+            queue.add(jsObjRequest)
+        }
+        val cookie = CookieManager.getInstance().getCookie(COOKIE_DOMAIN)
+        if (cookie != null) {
+            val tipserTokenPart = cookie.split(";").map { v -> v.trim() }
+                .find { v -> v.startsWith("$TIPSER_TOKEN_KEY=") }
+            if (tipserTokenPart != null) {
+                val tipserToken = tipserTokenPart.split("=")[1]
+
+                val jsObjRequest = object : StringRequest(
+                    Request.Method.GET,
+                    "$BASE_TIPSER_API_URL/v3/auth/token",
+                    Response.Listener<String> { response ->
+                        if (response == TOKEN_INVALID) {
+                            getNewToken()
+                        } else {
+                            CookieManager.getInstance()
+                                .setCookie(COOKIE_DOMAIN, "$TIPSER_TOKEN_KEY=$response;")
+                        }
+                    },
+                    Response.ErrorListener { e -> e.printStackTrace() }) {
+                    @Throws(AuthFailureError::class)
+                    override fun getHeaders(): Map<String, String> {
+                        val params = HashMap<String, String>()
+                        params["authorization"] = "Bearer $tipserToken"
+                        return params
+                    }
+                }
+                queue.add(jsObjRequest)
+            } else {
+                getNewToken()
+            }
+        } else {
+            getNewToken()
+        }
     }
 
     fun initWebView(activityContext: AppCompatActivity) {
-        val myWebView = WebView(activityContext)
-        myWebView.webViewClient = WebViewClient()
-        activityContext.setContentView(myWebView)
-        myWebView.settings.domStorageEnabled = true
-        myWebView.settings.javaScriptEnabled = true
-        myWebView.loadUrl("https://tipser.com")
+//        CookieManager.getInstance().setCookie(parsedURL.host, "")
+        this.webView = WebView(activityContext)
+
+        this.webView.webViewClient = WebViewClient()
+
+
+//        this.webView.visibility = View.GONE
+        activityContext.setContentView(this.webView)
+        this.webView.settings.domStorageEnabled = true
+        this.webView.settings.javaScriptEnabled = true
+        this.webView.loadUrl("https://tipser.com/checkout")
     }
 
     fun goToCheckout() {
@@ -28,6 +101,10 @@ class TipserSdk {
 
     fun directToCheckout() {
 
+    }
+
+    fun showCart() {
+        this.webView.loadUrl("https://www.tipser.com/checkout")
     }
 
 }
--- app/src/main/java/com/example/android_sdk_bootstrap/MainActivity.kt
+++ app/src/main/java/com/example/android_sdk_bootstrap/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.android_sdk_bootstrap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.webkit.CookieManager
 import android.webkit.WebView
+import android.widget.Button
 import com.example.android_sdk.TipserSdk
 
 
@@ -13,6 +16,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val sdk = TipserSdk()
 
+
+//        val addToCartButton = this.findViewById(R.id.button) as Button
+//        val showCart= this.findViewById(R.id.button2) as Button
+        sdk.init(this)
         sdk.initWebView(this)
+//        addToCartButton.setOnClickListener {v -> sdk.addToCart("5da5c5139af3ba00010b41bc") }
+//
+//        showCart.setOnClickListener {v -> sdk.showCart()}
     }
 }
